c996ec12bf20ad32e9d9930ce43ba3ff25d096c8
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2020-11-04 11:23:47 -0800

    Added testing, automation, improved incoming shout logic, NeonBot init (#7)
    * Update default domain to chatbotsforum.org
    Increment version
    
    * Add psutil requirement
    
    * Add psutil requirement (fix typo)
    
    * Add facilitator nicks param
    
    * Add unit testing
    
    * Fix unit test file locations
    
    * Troubleshoot automation
    
    * Troubleshoot automation klat-connector auth
    
    * Troubleshoot automation klat-connector auth
    
    * Troubleshoot automation shutdown
    
    * Update gitignore
    
    * Add more ChatBot testing
    
    * Update incoming shout logic to handle @Proctor from current conversation
    
    * Update testing to handle double digit test numbering
    
    * Fix NeonBot init order
    Fic testing typo
    
    * Increase timeout to isolate Klat API errors and not fail if nicks take too long

--- tests/chatbot_core_tests.py
+++ tests/chatbot_core_tests.py
@@ -11,66 +11,91 @@ from klat_connector import start_socket
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from chatbot_core import ChatBot, ConversationControls, ConversationState
 
-bot = ChatBot(start_socket("2222.us"), "chatbotsforum.org", "testrunner", "testpassword", True)
-
 
 class ChatbotCoreTests(unittest.TestCase):
-    @pytest.mark.timeout(30)
-    def test_1_initial_connection_settings(self):
-        bot.bot_type = "submind"
-        while not bot.ready:
+    bot = ChatBot(start_socket("2222.us"), "Private", "testrunner", "testpassword", True)
+    test_input = "prompt goes here"
+
+    @pytest.mark.timeout(10)
+    def test_01_initial_connection_settings(self):
+        self.bot.bot_type = "submind"
+        while not self.bot.ready:
             sleep(1)
-        self.assertEqual(bot.nick, "testrunner")
-        self.assertEqual(bot.logged_in, 2)
+        self.assertEqual(self.bot.nick, "testrunner")
+        self.assertEqual(self.bot.logged_in, 2)
 
-    @pytest.mark.timeout(30)
-    def test_2_submind_conversation_input(self):
-        test_input = "input test"
-        self.assertEqual(bot.state, ConversationState.IDLE)
-        bot.handle_incoming_shout("Proctor", f"testrunner {ConversationControls.RESP} "
-                                             f"{test_input} (for 0 seconds).", bot._cid, bot._dom,
-                                  datetime.now().strftime("%I:%M:%S %p"))
-        self.assertEqual(bot.active_prompt, test_input)
-        self.assertEqual(bot.state, ConversationState.RESP)
-        self.assertEqual(bot.chat_history[0][0], "testrunner", f"history={bot.chat_history}")
-        self.assertEqual(bot.chat_history[0][1], test_input)
+    @pytest.mark.timeout(10)
+    def test_02_submind_response(self):
+        self.assertEqual(self.bot.state, ConversationState.IDLE)
+        self.bot.handle_incoming_shout("Proctor", f"testrunner {ConversationControls.RESP} "
+                                                  f"{self.test_input} (for 0 seconds).", self.bot._cid, self.bot._dom,
+                                       datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(self.bot.active_prompt, self.test_input)
+        self.assertEqual(self.bot.state, ConversationState.RESP)
+        self.assertEqual(self.bot.chat_history[0][0], "testrunner", f"history={self.bot.chat_history}")
+        self.assertEqual(self.bot.chat_history[0][1], self.test_input)
+        self.assertEqual(len(self.bot.proposed_responses[self.test_input]), 0)
 
-    # TODO: Test bot responses here DM
+    @pytest.mark.timeout(10)
+    def test_03_other_submind_responses(self):
+        self.assertEqual(self.bot.state, ConversationState.RESP)
+        self.bot.handle_incoming_shout("Other", "Other Bot Response.", self.bot._cid, self.bot._dom,
+                                       datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(len(self.bot.proposed_responses[self.test_input]), 1)
+        self.bot.handle_incoming_shout("Another", "Another Bot Response.", self.bot._cid, self.bot._dom,
+                                       datetime.now().strftime("%I:%M:%S %p"))
 
-    @pytest.mark.timeout(30)
-    def test_3_submind_conversation_discussion(self):
-        # bot.active_prompt = "input test"
-        # bot.proposed_responses["input test"] = {}
-        bot.handle_incoming_shout("Proctor", f"{ConversationControls.DISC} 0 seconds.",
-                                  bot._cid, bot._dom, datetime.now().strftime("%I:%M:%S %p"))
-        self.assertEqual(ConversationState.DISC, bot.state)
+        self.assertIn("Other", self.bot.proposed_responses[self.test_input].keys())
+        self.assertIn("Other Bot Response.", self.bot.proposed_responses[self.test_input].values())
 
-    # TODO: Test bot conversation here DM
+    @pytest.mark.timeout(10)
+    def test_04_submind_discussion(self):
+        self.bot.handle_incoming_shout("Proctor", f"{ConversationControls.DISC} 0 seconds.",
+                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(ConversationState.DISC, self.bot.state)
 
-    @pytest.mark.timeout(30)
-    def test_4_submind_conversation_voting(self):
-        bot.handle_incoming_shout("Proctor", f"{ConversationControls.VOTE} 0 seconds.",
-                                  bot._cid, bot._dom, datetime.now().strftime("%I:%M:%S %p"))
-        self.assertEqual(ConversationState.VOTE, bot.state)
+    @pytest.mark.timeout(10)
+    def test_05_other_submind_discussion(self):
+        self.assertEqual(self.bot.state, ConversationState.DISC)
+        len_responses = len(self.bot.proposed_responses[self.test_input])
+        self.bot.handle_incoming_shout("Other", "Other Bot Discussion.", self.bot._cid, self.bot._dom,
+                                       datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(len(self.bot.proposed_responses[self.test_input]), len_responses,
+                         "Discussion counted as a response!")
 
-    # TODO: Test bot votes here DM
+    @pytest.mark.timeout(10)
+    def test_06_submind_conversation_voting(self):
+        self.bot.handle_incoming_shout("Proctor", f"{ConversationControls.VOTE} 0 seconds.",
+                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(ConversationState.VOTE, self.bot.state)
 
-    @pytest.mark.timeout(30)
-    def test_5_submind_conversation_pick(self):
-        bot.handle_incoming_shout("Proctor", ConversationControls.PICK,
-                                  bot._cid, bot._dom, datetime.now().strftime("%I:%M:%S %p"))
-        self.assertEqual(ConversationState.PICK, bot.state)
+    @pytest.mark.timeout(10)
+    def test_07_handle_votes(self):
+        len_responses = len(self.bot.proposed_responses[self.test_input])
+        self.assertEqual(self.bot.state, ConversationState.VOTE)
+        self.bot.handle_incoming_shout("Other", "I vote for testrunner", self.bot._cid, self.bot._dom,
+                                       datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(len(self.bot.proposed_responses[self.test_input]), len_responses,
+                         "Vote counted as a response!")
 
-    @pytest.mark.timeout(30)
-    def test_6_submind_conversation_idle(self):
-        bot.handle_incoming_shout("Proctor", "The selected response is testrunner: \"test response\"",
-                                  bot._cid, bot._dom, datetime.now().strftime("%I:%M:%S %p"))
-        self.assertEqual(ConversationState.IDLE, bot.state)
-        self.assertEqual(bot.selected_history, ["testrunner"])
+    @pytest.mark.timeout(10)
+    def test_08_submind_conversation_pick(self):
+        self.bot.handle_incoming_shout("Proctor", ConversationControls.PICK,
+                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(ConversationState.PICK, self.bot.state)
 
-    def test_7_shutdown_testing(self):
-        bot.socket.disconnect()
-        self.assertFalse(bot.socket.connected)
+    @pytest.mark.timeout(10)
+    def test_09_submind_conversation_idle(self):
+        self.bot.handle_incoming_shout("Proctor", "The selected response is testrunner: \"test response\"",
+                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+        self.assertEqual(ConversationState.IDLE, self.bot.state)
+        self.assertEqual(self.bot.selected_history, ["testrunner"])
+        self.assertEqual(self.bot.active_prompt, None)
+
+    @pytest.mark.timeout(10)
+    def test_10_shutdown_testing(self):
+        self.bot.socket.disconnect()
+        self.assertFalse(self.bot.socket.connected)
 
 
 if __name__ == '__main__':
