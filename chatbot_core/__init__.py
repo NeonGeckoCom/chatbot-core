f372c275fbf41592bbeccaaf46275e59aa24eeb5
Daniel McKnight <daniel@neongecko.com>
2020-11-20 14:30:48 -0800

    Update LOG for backwards compat

--- chatbot_core/__init__.py
+++ chatbot_core/__init__.py
@@ -59,6 +59,7 @@ class ChatBot(KlatApi):
     def __init__(self, socket: Socket, domain: str = "chatbotsforum.org",
                  username: str = None, password: str = None, on_server: bool = True):
         super(ChatBot, self).__init__(socket, domain)
+        global LOG
         # self.log.debug("Connector started")
         self.on_server = on_server
         self.start_domain = domain
@@ -71,6 +72,7 @@ class ChatBot(KlatApi):
         self.password = password
 
         self.log = make_logger(self.__class__.__name__)
+        LOG = self.log
         klat_timeout = time.time() + 30
         while not self.ready and time.time() < klat_timeout:
             time.sleep(1)
