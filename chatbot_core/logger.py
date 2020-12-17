147e5ca71641cc9522e23adf4d8dec82d640a830
Daniel McKnight <daniel@neongecko.com>
2020-12-17 14:32:26 -0800

    Fix git errors after reverting commit to Master

--- chatbot_core/logger.py
+++ chatbot_core/logger.py
@@ -19,9 +19,18 @@
 
 import logging
 
-fmt = '%(asctime)s - %(levelname)-8s - %(name)s:%(module)s:%(funcName)s:%(lineno)d - %(message)s'
-logging.basicConfig(level=logging.DEBUG, format=fmt, datefmt='%Y-%m-%d:%H:%M:%S')
-logging.getLogger("socketio.client").setLevel(logging.WARNING)
-logging.getLogger("engineio.client").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
+# fmt = '%(asctime)s - %(levelname)-8s - %(name)s:%(filename)s:%(module)s:%(funcName)s:%(lineno)d - %(message)s'
+# logging.basicConfig(level=logging.DEBUG, format=fmt, datefmt='%Y-%m-%d:%H:%M:%S')
 LOG = logging.getLogger("chatbots")
+# logging.getLogger("socketio.client").setLevel(logging.WARNING)
+# logging.getLogger("engineio.client").setLevel(logging.WARNING)
+# logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+def make_logger(name, level=logging.DEBUG):
+    """
+    Create a logger with the specified name (used to create bot loggers)
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    return logging.getLogger(name)
