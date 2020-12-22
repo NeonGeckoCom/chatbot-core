f6b47d42f225387cac38ffe43ef0e39410670128
NeonKirill <kirill@neon.ai>
2020-12-22 12:29:50 -0500

    added generic function to find closest answers

--- chatbot_core/__init__.py
+++ chatbot_core/__init__.py
@@ -33,8 +33,60 @@ from klat_connector.klat_api import KlatApi
 from klat_connector import start_socket  # Leave for extending classes to use without explicit klat_connector import
 from chatbot_core.logger import make_logger
 from mycroft_bus_client import Message, MessageBusClient
+from nltk.translate.bleu_score import sentence_bleu
+from nltk import word_tokenize
+import jellyfish
 
 LOG = make_logger("chatbot")
+
+
+def find_closest_answer(algorithm: str = 'random', sentence: str = None, options: dict = None):
+    """
+        Handles an incoming shout into the current conversation
+        :param algorithm: algorithm considered
+        :param sentence: base sentence
+        :param options: options to pick best one from
+    """
+    if not sentence:
+        LOG.warning('Empty sentence supplied')
+        return sentence
+    if not options or len(options.keys()) == 0:
+        LOG.warning('No options provided')
+        return sentence
+    if algorithm == 'random':
+        closest_answer = random.choice(options)
+    elif algorithm == 'bleu_score':
+        bleu_scores = []
+        response_tokenized = word_tokenize(sentence.lower())
+        for option in options.keys():
+            opinion_tokenized = word_tokenize(options[option].lower())
+            if len(opinion_tokenized) > 0:
+                if min(len(response_tokenized), len(opinion_tokenized)) < 4:
+                    weighting = 1.0 / min(len(response_tokenized), len(opinion_tokenized))
+                    weights = tuple([weighting] * min(len(response_tokenized), len(opinion_tokenized)))
+                else:
+                    weights = (0.25, 0.25, 0.25, 0.25)
+                bleu_scores.append(
+                    (option, sentence_bleu([response_tokenized], opinion_tokenized, weights=weights)))
+        max_score = max([x[1] for x in bleu_scores]) if len(bleu_scores) > 0 else 0
+        closest_answer = random.choice(list(filter(lambda x: x[1] == max_score, bleu_scores)))[0]
+        LOG.info(f'Closest answer is {closest_answer}')
+    elif algorithm == 'damerau_levenshtein_distance':
+        closest_distance = None
+        closest_answer = None
+        try:
+            for option in options.items():
+                distance = jellyfish.damerau_levenshtein_distance(option[1], sentence)
+                if not closest_distance or closest_distance > distance:
+                    closest_distance = distance
+                    closest_answer = option[0]
+            LOG.info(f'Closest answer is {closest_answer}')
+        except Exception as e:
+            LOG.error(e)
+    else:
+        LOG.error(f'Unknown algorithm supplied:{algorithm}')
+        return sentence
+    return closest_answer
 
 
 class ConversationControls:
