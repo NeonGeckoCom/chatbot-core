3cec0981ed6903ba9c34a231ceb1bdc572362b47
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2020-12-22 13:09:30 -0800

    Add back decorator mixin from #88 (#93)
    Timeout test_start_bot fn

--- tests/chatbot_core_tests.py
+++ tests/chatbot_core_tests.py
@@ -39,8 +39,8 @@ class ChatbotCoreTests(unittest.TestCase):
     def test_02_submind_response(self):
         self.assertEqual(self.bot.state, ConversationState.IDLE)
         self.bot.handle_shout("Proctor", f"testrunner {ConversationControls.RESP} "
-                                                  f"{self.test_input} (for 0 seconds).", self.bot._cid, self.bot._dom,
-                                       datetime.now().strftime("%I:%M:%S %p"))
+                                         f"{self.test_input} (for 0 seconds).", self.bot._cid, self.bot._dom,
+                              datetime.now().strftime("%I:%M:%S %p"))
         self.assertEqual(self.bot.active_prompt, self.test_input)
         self.assertEqual(self.bot.state, ConversationState.RESP)
         self.assertEqual(self.bot.request_history[0][0], "testrunner", f"history={self.bot.request_history}")
@@ -51,10 +51,10 @@ class ChatbotCoreTests(unittest.TestCase):
     def test_03_other_submind_responses(self):
         self.assertEqual(self.bot.state, ConversationState.RESP)
         self.bot.handle_shout("Other", "Other Bot Response.", self.bot._cid, self.bot._dom,
-                                       datetime.now().strftime("%I:%M:%S %p"))
+                              datetime.now().strftime("%I:%M:%S %p"))
         self.assertEqual(len(self.bot.proposed_responses[self.test_input]), 1)
         self.bot.handle_shout("Another", "Another Bot Response.", self.bot._cid, self.bot._dom,
-                                       datetime.now().strftime("%I:%M:%S %p"))
+                              datetime.now().strftime("%I:%M:%S %p"))
 
         self.assertIn("Other", self.bot.proposed_responses[self.test_input].keys())
         self.assertIn("Other Bot Response.", self.bot.proposed_responses[self.test_input].values())
@@ -62,7 +62,7 @@ class ChatbotCoreTests(unittest.TestCase):
     @pytest.mark.timeout(10)
     def test_04_submind_discussion(self):
         self.bot.handle_shout("Proctor", f"{ConversationControls.DISC} 0 seconds.",
-                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+                              self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
         self.assertEqual(ConversationState.DISC, self.bot.state)
 
     @pytest.mark.timeout(10)
@@ -77,7 +77,7 @@ class ChatbotCoreTests(unittest.TestCase):
     @pytest.mark.timeout(10)
     def test_06_submind_conversation_voting(self):
         self.bot.handle_shout("Proctor", f"{ConversationControls.VOTE} 0 seconds.",
-                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+                              self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
         self.assertEqual(ConversationState.VOTE, self.bot.state)
 
     @pytest.mark.timeout(10)
@@ -92,13 +92,13 @@ class ChatbotCoreTests(unittest.TestCase):
     @pytest.mark.timeout(10)
     def test_08_submind_conversation_pick(self):
         self.bot.handle_shout("Proctor", ConversationControls.PICK,
-                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+                              self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
         self.assertEqual(ConversationState.PICK, self.bot.state)
 
     @pytest.mark.timeout(10)
     def test_09_submind_conversation_idle(self):
         self.bot.handle_shout("Proctor", "The selected response is testrunner: \"test response\"",
-                                       self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
+                              self.bot._cid, self.bot._dom, datetime.now().strftime("%I:%M:%S %p"))
         self.assertEqual(ConversationState.IDLE, self.bot.state)
         self.assertEqual(self.bot.selected_history, ["testrunner"])
         self.assertEqual(self.bot.active_prompt, None)
@@ -179,10 +179,11 @@ class ChatbotCoreTests(unittest.TestCase):
         self.assertIsInstance(e, synchronize.Event)
         # self.assertFalse(e.is_set())
         e.set()
-        while e.is_set():
+        timeout = time.time() + 10
+        while e.is_set() and time.time() < timeout:
             print("...")
             time.sleep(2)
-        # self.assertFalse(e.is_set())
+        self.assertFalse(e.is_set())
         # self.assertTrue(t.is_alive())
         print(f"Terminating... {t.is_alive()}")
         t.terminate()
