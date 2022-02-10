a5c36d92ec7f412e3cb395ec12568473963ffb9c
NeonKirill <kirill.grim@gmail.com>
2022-02-10 19:45:17 +0200

    import fix

--- chatbot_core/utils/conversation_utils.py
+++ chatbot_core/utils/conversation_utils.py
@@ -3,7 +3,7 @@ from itertools import cycle
 
 def create_conversation_cycle() -> cycle:
     """Cycle through conversation states"""
-    from chatbot_core import ConversationState
+    from chatbot_core.utils import ConversationState
 
     return cycle([ConversationState.RESP,
                   ConversationState.DISC,
