f52450a0c21e48fc154fe7340c0567023bdd985e
Daniel McKnight <daniel@neon.ai>
2023-11-29 16:43:37 -0800

    init log to reduce debug output

--- chatbot_core/utils/bot_utils.py
+++ chatbot_core/utils/bot_utils.py
@@ -661,6 +661,9 @@ def run_mq_bot(chatbot_name: str, vhost: str = '/chatbots',
     @param init_kwargs: extra kwargs to pass to chatbot `__init__` method
     @returns: Started ChatBotV2 instance
     """
+    from neon_utils.log_utils import LOG, init_log
+    init_log({"level_overrides": {"error": ['pika'],
+                                  "warning": ["filelock"]}})
     os.environ['CHATBOT_VERSION'] = 'v2'
     run_kwargs = run_kwargs or dict()
     init_kwargs = init_kwargs or dict()
