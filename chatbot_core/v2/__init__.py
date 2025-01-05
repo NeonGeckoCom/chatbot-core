43d7985e469cdb0224de361d8c9923825c713941
kgrim <kirill.grim@gmail.com>
2025-01-05 13:09:06 +0100

    - Introduced "FACILITATOR" typed bot - Added handling of "announce_invitation" instead of "skip_announcement"

--- chatbot_core/v2/__init__.py
+++ chatbot_core/v2/__init__.py
@@ -70,12 +70,12 @@ class ChatBot(KlatAPIMQ, ChatBotABC):
     def handle_invite(self, body: dict):
         """Handles incoming request to chatbot"""
         new_cid = body.pop('cid', None)
-        skip_announcement = body.pop('skip_announcement', False)
+        announce_invitation = body.pop('announce_invitation', True)
         self.log.info(f'Received invitation to cid: {new_cid}')
         if new_cid and not self.current_conversations.get(new_cid, None):
             self.current_conversations[new_cid] = body
             self.set_conversation_state(new_cid, ConversationState.IDLE)
-            if not skip_announcement:
+            if announce_invitation:
                 self.send_announcement(f'{self.nick.split("-")[0]} joined', new_cid)
 
     def get_conversation_state(self, cid) -> ConversationState:
