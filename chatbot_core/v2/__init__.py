d7fc0f4f9251602e22529325d89e4f0e2d88c493
Daniel McKnight <daniel@neon.ai>
2025-03-26 17:03:31 -0700

    Fix typo

--- chatbot_core/v2/__init__.py
+++ chatbot_core/v2/__init__.py
@@ -314,7 +314,7 @@ class ChatBot(KlatAPIMQ, ChatBotABC):
         is_message_from_proctor = self._user_is_proctor(message_sender)
 
 
-        if conversation_state in message_data:
+        if "conversation_state" in message_data:
             self.set_conversation_state(cid, message_data['conversation_state'])
             self.log.info(f"Conversation state from message data: {conversation_state}")
         elif is_message_from_proctor:
